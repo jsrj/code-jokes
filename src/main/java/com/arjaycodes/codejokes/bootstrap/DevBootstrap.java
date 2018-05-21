23d6e6c48684fc969a0fba46b09019ea7ab228ad
Arjay Jones <robert.jones@arjaycodes.com>
2018-05-21 15:05:55 -0400

    added method to seed h2 with some jokes

--- src/main/java/com/arjaycodes/codejokes/bootstrap/DevBootstrap.java
+++ src/main/java/com/arjaycodes/codejokes/bootstrap/DevBootstrap.java
@@ -1,4 +1,45 @@
 package com.arjaycodes.codejokes.bootstrap;
 
-public class DevBootstrap {
+import com.arjaycodes.codejokes.entity.Joke;
+import com.arjaycodes.codejokes.repository.JokeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private JokeRepository jokeRepo;
+
+    public DevBootstrap(JokeRepository jokeRepo) {
+        this.jokeRepo = jokeRepo;
+    }
+
+
+    public void initData() {
+        Joke joke1 = new Joke();
+        Joke joke2 = new Joke();
+        Joke joke3 = new Joke();
+        Joke joke4 = new Joke();
+
+        joke1.setJokeName("Joke 1");
+        joke1.setJokeContent("Haha");
+        jokeRepo.save(joke1);
+
+        joke2.setJokeName("Joke 2");
+        joke2.setJokeName("Very Funny");
+        jokeRepo.save(joke2);
+
+        joke3.setJokeName("Joke 3");
+        joke3.setJokeContent("Knock knock");
+        jokeRepo.save(joke3);
+
+        joke4.setJokeName("Joke 4");
+        joke4.setJokeContent("No Seriously");
+        jokeRepo.save(joke4);
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        this.initData();
+    }
 }
