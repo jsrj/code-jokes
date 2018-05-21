298439729589d6a7e4758bb232bdd1a9e07e35b5
Arjay Jones <robert.jones@arjaycodes.com>
2018-05-21 16:32:47 -0400

    changed name passed into view

--- src/main/java/com/arjaycodes/codejokes/CodejokesApplication.java
+++ src/main/java/com/arjaycodes/codejokes/CodejokesApplication.java
@@ -1,6 +1,7 @@
 package com.arjaycodes.codejokes;
 
 import com.arjaycodes.codejokes.bootstrap.DevBootstrap;
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
