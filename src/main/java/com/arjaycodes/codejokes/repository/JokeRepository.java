1ba2ac55e9908ede7b6d285af151bc3bec3542d0
Arjay Jones <robert.jones@arjaycodes.com>
2018-05-21 15:35:14 -0400

    added missing @Component annotation

--- src/main/java/com/arjaycodes/codejokes/repository/JokeRepository.java
+++ src/main/java/com/arjaycodes/codejokes/repository/JokeRepository.java
@@ -3,5 +3,4 @@ package com.arjaycodes.codejokes.repository;
 import com.arjaycodes.codejokes.entity.Joke;
 import org.springframework.data.repository.CrudRepository;
 
-public interface JokeRepository extends CrudRepository<Joke, Long> {
-}
+public interface JokeRepository extends CrudRepository<Joke, Long> {}
