8aea188939de1fe4473a77774cf95bef104925f5
Arjay Jones <robert.jones@arjaycodes.com>
2018-05-21 15:34:19 -0400

    added missing @Component annotation

--- src/main/java/com/arjaycodes/codejokes/bootstrap/DevBootstrap.java
+++ src/main/java/com/arjaycodes/codejokes/bootstrap/DevBootstrap.java
@@ -2,44 +2,37 @@ package com.arjaycodes.codejokes.bootstrap;
 
 import com.arjaycodes.codejokes.entity.Joke;
 import com.arjaycodes.codejokes.repository.JokeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
-    private JokeRepository jokeRepo;
+    private JokeRepository jokeRepository;
 
-    public DevBootstrap(JokeRepository jokeRepo) {
-        this.jokeRepo = jokeRepo;
-    }
-
-
-    public void initData() {
-        Joke joke1 = new Joke();
-        Joke joke2 = new Joke();
-        Joke joke3 = new Joke();
-        Joke joke4 = new Joke();
-
-        joke1.setJokeName("Joke 1");
-        joke1.setJokeContent("Haha");
-        jokeRepo.save(joke1);
-
-        joke2.setJokeName("Joke 2");
-        joke2.setJokeName("Very Funny");
-        jokeRepo.save(joke2);
-
-        joke3.setJokeName("Joke 3");
-        joke3.setJokeContent("Knock knock");
-        jokeRepo.save(joke3);
-
-        joke4.setJokeName("Joke 4");
-        joke4.setJokeContent("No Seriously");
-        jokeRepo.save(joke4);
+    public DevBootstrap(JokeRepository jokeRepository) {
+        this.jokeRepository = jokeRepository;
     }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        this.initData();
+        initData();
+    }
+
+    private void initData() {
+        Joke joke1 = new Joke("Joke 1", "Haha");
+        jokeRepository.save(joke1);
+
+        Joke joke2 = new Joke("Joke 2", "Very Funny");
+        jokeRepository.save(joke2);
+
+        Joke joke3 = new Joke("Joke 3", "Knock knock");
+        jokeRepository.save(joke3);
+
+        Joke joke4 = new Joke("Joke 4", "No Seriously");
+        jokeRepository.save(joke4);
+
+        System.out.println("Created new jokes");
     }
 }
