067daf0e5b7fc8578754a360a281444e9febe56c
Arjay Jones <robert.jones@arjaycodes.com>
2018-05-22 10:07:58 -0400

    fixed borked config paths

--- src/main/java/com/arjaycodes/codejokes/CodejokesApplication.java
+++ src/main/java/com/arjaycodes/codejokes/CodejokesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.arjaycodes.codejokes.service"})
+@ComponentScan(basePackages = {"com.arjaycodes.codejokes"})
 public class CodejokesApplication {
 
 	public static void main(String[] args) {
