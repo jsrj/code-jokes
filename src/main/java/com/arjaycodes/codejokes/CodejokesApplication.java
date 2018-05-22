a6892403b2002c5a19ba16fa716ba2d0977d920d
Arjay Jones <robert.jones@arjaycodes.com>
2018-05-22 10:19:03 -0400

    created an xml version of config just because. But its XML so itll be gotten rid of.

--- src/main/java/com/arjaycodes/codejokes/CodejokesApplication.java
+++ src/main/java/com/arjaycodes/codejokes/CodejokesApplication.java
@@ -3,9 +3,10 @@ package com.arjaycodes.codejokes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.arjaycodes.codejokes"})
+@ImportResource(locations = {"Joke-Config.xml"})
 public class CodejokesApplication {
 
 	public static void main(String[] args) {
